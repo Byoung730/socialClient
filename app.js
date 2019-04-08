406dfa6c8ee04348d9e17dd842d64afb0c8e6d62
Brian <brian.young@maritz.com>
2019-04-08 13:20:39 -0500

    add location route

--- app.js
+++ app.js
@@ -24,13 +24,62 @@ const session = driver.session();
 
 // Home route
 app.get("/", (req, res) => {
+  session.run("MATCH(n:Person) RETURN n").then(result => {
+    const personArr = [];
+    result.records.forEach(record => {
+      // console.log(record._fields[0])
+      personArr.push({
+        id: record._fields[0].identity.low,
+        name: record._fields[0].properties.name
+      });
+    });
+
+    session
+      .run("MATCH(n:Location) RETURN n")
+      .then(result2 => {
+        const locationArr = [];
+        result2.records.forEach(record => {
+          locationArr.push(record._fields[0].properties);
+        });
+        res.render("index", {
+          persons: personArr,
+          locations: locationArr
+        });
+      })
+      .catch(e => console.log(e));
+  });
+});
+
+// Add Person
+app.post("/person/add", (req, res) => {
+  const name = req.body.name;
+
   session
-    .run("MATCH(n) RETURN n")
+    .run("CREATE(n:Person {name:{nameParam}}) RETURN n.name", {
+      nameParam: name
+    })
     .then(result => {
-      result.records.forEach(record => console.log(record._fields[0]));
+      res.redirect("/");
+      session.close;
     })
     .catch(e => console.log(e));
-  res.render("index");
+});
+
+// Add Location
+app.post("/location/add", (req, res) => {
+  const name = req.body.name;
+  const state = req.body.state;
+
+  session
+    .run("CREATE(n:Location {name:{nameParam}, state:{stateParam}}) RETURN n", {
+      nameParam: name,
+      stateParam: state
+    })
+    .then(result => {
+      res.redirect("/");
+      session.close;
+    })
+    .catch(e => console.log(e));
 });
 
 app.listen(3000);
