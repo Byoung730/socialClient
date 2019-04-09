2647720edbdb8f95e3bfd5adcf6bbe3ccfa0e5ab
Brian <brian.young@maritz.com>
2019-04-09 09:13:54 -0500

    individual person pages

--- app.js
+++ app.js
@@ -82,6 +82,102 @@ app.post("/location/add", (req, res) => {
     .catch(e => console.log(e));
 });
 
+// Friends Connect Route
+app.post("/friends/connect", (req, res) => {
+  const name1 = req.body.name1;
+  const name2 = req.body.name2;
+
+  session
+    .run(
+      "MATCH(a:Person {name:{nameParam1}}),(b:Person {name:{nameParam2}}) MERGE(a)-[r:FRIENDS]->(b) RETURN a,b",
+      {
+        nameParam1: name1,
+        nameParam2: name2
+      }
+    )
+    .then(result => {
+      res.redirect("/");
+      session.close;
+    })
+    .catch(e => console.log(e));
+});
+
+// Add Birthplace Route
+app.post("/person/born/add", (req, res) => {
+  const name1 = req.body.name1;
+  const name2 = req.body.name2;
+  const state = req.body.state;
+  const year = req.body.year;
+
+  session
+    .run(
+      "MATCH(a:Person {name:{nameParam1}}),(b:Location {name:{nameParam2}, state:{stateParam}}) MERGE(a)-[r:BORN_IN {year:{yearParam}}]->(b) RETURN a,b",
+      {
+        nameParam1: name1,
+        nameParam2: name2,
+        stateParam: state,
+        yearParam: year
+      }
+    )
+    .then(result => {
+      res.redirect("/");
+      session.close;
+    })
+    .catch(e => console.log(e));
+});
+
+// Person Route
+app.get("/person/:id", (req, res) => {
+  const id = req.params.id;
+
+  session
+    .run("MATCH(a:Person) WHERE id(a)=toInt({idParam}) RETURN a.name as name", {
+      idParam: id
+    })
+    .then(result => {
+      const name = result.records[0].get("name");
+
+      session
+        .run(
+          "OPTIONAL MATCH(a:Person)-[r:BORN_IN]-(b:Location) WHERE id(a)=toInt({idParam}) RETURN b.name as city, b.state as state",
+          { idParam: id }
+        )
+        .then(result2 => {
+          const city = result2.records[0].get("city");
+          const state = result2.records[0].get("state");
+
+          session
+            .run(
+              "OPTIONAL MATCH (a:Person)-[r:Friends]-(b:Person) WHERE id(a)=toInt({idParam}) RETURN b",
+              { idParam: id }
+            )
+            .then(result3 => {
+              const friendsArr = [];
+
+              result3.records.forEach(record => {
+                if (record._fields[0] !== null) {
+                  friendsArr.push({
+                    id: record._fields[0].identity.low,
+                    name: record._fields[0].properties.name
+                  });
+                }
+              });
+
+              res.render("person", {
+                id: id,
+                name: name,
+                city: city,
+                state: state,
+                friends: friendsArr
+              });
+
+              session.close();
+            })
+            .catch(e => console.log(e));
+        });
+    });
+});
+
 app.listen(3000);
 
 console.log("Server started on port 3000");
